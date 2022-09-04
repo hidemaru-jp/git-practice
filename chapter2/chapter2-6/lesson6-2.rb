5636fafba20f6849cc77aa7b430047b335ecb28a
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-09-04 04:00:03 +0000

    [Finish]Exercise2-6

---
+++ chapter2/chapter2-6/lesson6-2.rb
@@ -0,0 +1,22 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+
+n =gets.to_i
+
+for i in 1..n do
+  
+  
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "a+b=#{a + b}"
+puts "a-b=#{a - b}"
+puts "a*b=#{a * b}"
+puts "a/b=#{a / b}"
+
+  
+end 
+
+puts "計算を終了します"
